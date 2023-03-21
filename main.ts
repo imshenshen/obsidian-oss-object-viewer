b05598b51ffff24ff044ca811b74f224155f1eb7
shenshen <im.shenshen@qq.com>
2023-03-21 17:17:38 +0800

    init project

--- main.ts
+++ main.ts
@@ -1,81 +1,63 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {App, MarkdownRenderer, MarkdownView, Modal, Plugin, PluginSettingTab, Setting} from 'obsidian';
+import * as AWS from 'aws-sdk';
+import * as aliOss from 'ali-oss';
 
 // Remember to rename these classes and interfaces!
 
-interface MyPluginSettings {
-	mySetting: string;
+type OssType = 'aliyun' | 'aws';
+interface OssObjectViewerSettings {
+	type: OssType
+	bucket?: string;
+	region: string;
+	accessKeyId: string;
+	secretAccessKey: string
 }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+const DEFAULT_SETTINGS: OssObjectViewerSettings = {
+	type: 'aliyun',
+	region: '',
+	accessKeyId: '',
+	secretAccessKey: ''
 }
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+export default class OssObjectViewer extends Plugin {
+	settings: OssObjectViewerSettings;
 
 	async onload() {
+
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
-
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+		this.addSettingTab(new OssObjectViewerSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
+		const bucket = this.settings.bucket
+		let sdkInstance: aliOss;
+
+		if (this.settings.type === 'aliyun') {
+			sdkInstance = new aliOss({
+				region: this.settings.region,
+				accessKeyId: this.settings.accessKeyId,
+				accessKeySecret: this.settings.secretAccessKey
+			})
+		} else if (this.settings.type === 'aws') {
+		}
+
+
+		this.registerMarkdownCodeBlockProcessor('', (source, el, ctx) => {
+			debugger
+			const regex = /!\[(.*?)\]\((.*?)\)/g;
+			const expiration = 3600; // The expiration time for the presigned URL, in seconds
+
+			const newSource = source.replace(regex, (match, p1, p2) => {
+				const presignedUrl = sdkInstance.signatureUrl(`${bucket}/${p2}`,{
+					expires : expiration,
+				});
+				return `![${p1}](${presignedUrl})`;
+			});
+			debugger
+
+			// el.innerHTML = MarkdownRenderer.renderMarkdown(newSource, ctx);
 		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -91,26 +73,10 @@ export default class MyPlugin extends Plugin {
 	}
 }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
+class OssObjectViewerSettingTab extends PluginSettingTab {
+	plugin: OssObjectViewer;
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
+	constructor(app: App, plugin: OssObjectViewer) {
 		super(app, plugin);
 		this.plugin = plugin;
 	}
@@ -120,18 +86,38 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for OssObjectViewer plugin.'});
 
 		new Setting(containerEl)
 			.setName('Setting #1')
 			.setDesc('It\'s a secret')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('Enter your bucket region')
+				.setValue(this.plugin.settings.region || '')
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.region = value;
 					await this.plugin.saveSettings();
+				}))
+			.addText(component => component
+				.setPlaceholder('Enter you bucket name')
+				.setValue(this.plugin.settings.bucket || '')
+				.onChange(async (value) => {
+					this.plugin.settings.bucket = value;
+					return this.plugin.saveSettings()
+				}))
+			.addText(component => component
+				.setPlaceholder('Enter you accessKeyId')
+				.setValue(this.plugin.settings.accessKeyId || '')
+				.onChange(async (value) => {
+					this.plugin.settings.accessKeyId = value;
+					return this.plugin.saveSettings()
+				}))
+			.addText(component => component
+				.setPlaceholder('Enter you secretAccessKey')
+				.setValue(this.plugin.settings.secretAccessKey || '')
+				.onChange(async (value) => {
+					this.plugin.settings.secretAccessKey = value;
+					return this.plugin.saveSettings()
 				}));
 	}
 }
